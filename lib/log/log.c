516c1522ad24100cb8a7044f81f3e8d21c35e531
Crane Chu <cranechu@gmail.com>
2020-06-19 16:25:48 +0800

    buffer dump with the name in header

--- lib/log/log.c
+++ lib/log/log.c
@@ -184,7 +184,7 @@ fdump(FILE *fp, const char *label, const uint8_t *buf, size_t len, size_t base)
 	size_t total;
 	unsigned int idx;
 
-	//fprintf(fp, "%s\n", label);
+	fprintf(fp, "%s\n", label);
 
 	memset(buf16, 0, sizeof buf16);
 	total = 0;
